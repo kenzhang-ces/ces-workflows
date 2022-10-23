f3d3dbc2f765ca1794bd5597c741d8668fe58c89
Alexander Constanti <Alex.Constanti@coexservices.com.au>
2022-10-23 16:53:41 +1100

    SPLT-570: Checkout workflows repo.

--- .github/scripts/slack-cut-release-branch-msg.sh
+++ .github/scripts/slack-cut-release-branch-msg.sh
@@ -1,15 +1,15 @@
-#!/bin/bash -eu
+#!/bin/bash -eux
 
 REPO_NAME=$1
 SLACK_WEBHOOK_URL=$2
 
-CUR_RELEASE_BRANCH=$(git branch -r| grep -Eo "release_.*" | sort | tail -1)
+CUR_RELEASE_BRANCH=$(git branch -r| grep -Eo "origin/release_.*" | sort | tail -1)
 PRE_RELEASE_BRANCH=$([ "$(git branch -r| grep -Eo "release_.*" | sort | tail -2 | wc -l)" -eq '2' ] &&
-  git branch -r| grep -Eo "release_.*" | sort | tail -2 | head -1 ||
+  git branch -r| grep -Eo "origin/release_.*" | sort | tail -2 | head -1 ||
   git rev-list --max-parents=0 HEAD)
-VERSION=$(git describe --tags CUR_RELEASE_BRANCH)
+VERSION=$(git describe --tags "$CUR_RELEASE_BRANCH")
 
-COMMITS=$(git log --left-right --pretty=format:'%s (%ar) <%an>' --abbrev-commit --date=relative origin/$PRE_RELEASE_BRANCH...origin/$CUR_RELEASE_BRANCH)
+COMMITS=$(git log --left-right --pretty=format:'%s (%ar) <%an>' --abbrev-commit --date=relative $PRE_RELEASE_BRANCH...$CUR_RELEASE_BRANCH)
 COMMITS=${COMMITS//$'\n'/\\n\\n}
 PAYLOAD="{
   \"blocks\": [
@@ -33,7 +33,7 @@ PAYLOAD="{
       \"fields\": [
         {
           \"type\": \"mrkdwn\",
-          \"text\": \"*branch:* <https://github.com/$REPO_NAME/releases/tag/$VERSION|$CUR_RELEASE_BRANCH>\"
+          \"text\": \"*release branch:* <https://github.com/$REPO_NAME/tree/${CUR_RELEASE_BRANCH//origin\//}|$CUR_RELEASE_BRANCH>\"
         },
         {
           \"type\": \"mrkdwn\",
@@ -41,7 +41,7 @@ PAYLOAD="{
         },
         {
           \"type\": \"mrkdwn\",
-          \"text\": \"*version:* <https://github.com/$REPO_NAME/releases/tag/$VERSION|$VERSION>\"
+          \"text\": \"*currnet release version:* <https://github.com/$REPO_NAME/releases/tag/$VERSION|$VERSION>\"
         }
       ]
     },
